23035b487c9743928425cfb466d59a63cf2f56ac
walter otero torres <walteroterotorres2003@gmail.com>
2023-05-06 21:26:54 -0500

    Capa cargar y guardar

--- Plantilla examen/VisualizarEmplezamientoySinEmplezamiento.Designer.cs
+++ Plantilla examen/VisualizarEmplezamientoySinEmplezamiento.Designer.cs
@@ -29,10 +29,14 @@
         private void InitializeComponent()
         {
             this.dtviewvisualizarEmplazamiento = new System.Windows.Forms.DataGridView();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dtviewsinEmplazamiento = new System.Windows.Forms.DataGridView();
             this.btnsalirvisualizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbcobroemplazamiento = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbcobrosinemplazamiento = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dtviewvisualizarEmplazamiento)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtviewsinEmplazamiento)).BeginInit();
             this.SuspendLayout();
             // 
             // dtviewvisualizarEmplazamiento
@@ -45,19 +49,19 @@
             this.dtviewvisualizarEmplazamiento.Size = new System.Drawing.Size(550, 201);
             this.dtviewvisualizarEmplazamiento.TabIndex = 0;
             // 
-            // dataGridView1
+            // dtviewsinEmplazamiento
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(75, 290);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(550, 201);
-            this.dataGridView1.TabIndex = 1;
+            this.dtviewsinEmplazamiento.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtviewsinEmplazamiento.Location = new System.Drawing.Point(75, 236);
+            this.dtviewsinEmplazamiento.Name = "dtviewsinEmplazamiento";
+            this.dtviewsinEmplazamiento.RowHeadersWidth = 51;
+            this.dtviewsinEmplazamiento.RowTemplate.Height = 24;
+            this.dtviewsinEmplazamiento.Size = new System.Drawing.Size(550, 201);
+            this.dtviewsinEmplazamiento.TabIndex = 1;
             // 
             // btnsalirvisualizar
             // 
-            this.btnsalirvisualizar.Location = new System.Drawing.Point(662, 359);
+            this.btnsalirvisualizar.Location = new System.Drawing.Point(736, 380);
             this.btnsalirvisualizar.Name = "btnsalirvisualizar";
             this.btnsalirvisualizar.Size = new System.Drawing.Size(126, 57);
             this.btnsalirvisualizar.TabIndex = 2;
@@ -65,27 +69,73 @@
             this.btnsalirvisualizar.UseVisualStyleBackColor = true;
             this.btnsalirvisualizar.Click += new System.EventHandler(this.btnsalirvisualizar_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(722, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CobrototalconEmplazamiento";
+            // 
+            // lbcobroemplazamiento
+            // 
+            this.lbcobroemplazamiento.AutoSize = true;
+            this.lbcobroemplazamiento.Location = new System.Drawing.Point(722, 93);
+            this.lbcobroemplazamiento.Name = "lbcobroemplazamiento";
+            this.lbcobroemplazamiento.Size = new System.Drawing.Size(27, 16);
+            this.lbcobroemplazamiento.TabIndex = 4;
+            this.lbcobroemplazamiento.Text = "NN";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(722, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "CobrototalsinEmplazamiento";
+            // 
+            // lbcobrosinemplazamiento
+            // 
+            this.lbcobrosinemplazamiento.AutoSize = true;
+            this.lbcobrosinemplazamiento.Location = new System.Drawing.Point(722, 271);
+            this.lbcobrosinemplazamiento.Name = "lbcobrosinemplazamiento";
+            this.lbcobrosinemplazamiento.Size = new System.Drawing.Size(27, 16);
+            this.lbcobrosinemplazamiento.TabIndex = 6;
+            this.lbcobrosinemplazamiento.Text = "NN";
+            // 
             // VisualizarEmplezamientoySinEmplezamiento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 534);
+            this.ClientSize = new System.Drawing.Size(1005, 534);
+            this.Controls.Add(this.lbcobrosinemplazamiento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbcobroemplazamiento);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnsalirvisualizar);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dtviewsinEmplazamiento);
             this.Controls.Add(this.dtviewvisualizarEmplazamiento);
             this.Name = "VisualizarEmplezamientoySinEmplezamiento";
             this.Text = "VisualizarEmplezamientoySinEmplezamiento";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.VisualizarEmplezamientoySinEmplezamiento_FormClosing);
+            this.Load += new System.EventHandler(this.VisualizarEmplezamientoySinEmplezamiento_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dtviewvisualizarEmplazamiento)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtviewsinEmplazamiento)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dtviewvisualizarEmplazamiento;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dtviewsinEmplazamiento;
         private System.Windows.Forms.Button btnsalirvisualizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbcobroemplazamiento;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbcobrosinemplazamiento;
     }
 }
--- Plantilla examen/visualizartotaldeclarado.Designer.cs
+++ Plantilla examen/visualizartotaldeclarado.Designer.cs
@@ -28,32 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dtviewtodo = new System.Windows.Forms.DataGridView();
             this.btnsalirIvisualizarTotal = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbcobroconemplazamiento = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbcobrosinemplazamiento = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtviewtodo)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dtviewtodo
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(142, 47);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(642, 202);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // dataGridView2
-            // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(142, 281);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.RowHeadersWidth = 51;
-            this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(642, 202);
-            this.dataGridView2.TabIndex = 1;
+            this.dtviewtodo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtviewtodo.Location = new System.Drawing.Point(142, 47);
+            this.dtviewtodo.Name = "dtviewtodo";
+            this.dtviewtodo.RowHeadersWidth = 51;
+            this.dtviewtodo.RowTemplate.Height = 24;
+            this.dtviewtodo.Size = new System.Drawing.Size(642, 202);
+            this.dtviewtodo.TabIndex = 0;
             // 
             // btnsalirIvisualizarTotal
             // 
@@ -65,27 +57,70 @@
             this.btnsalirIvisualizarTotal.UseVisualStyleBackColor = true;
             this.btnsalirIvisualizarTotal.Click += new System.EventHandler(this.btnsalirIvisualizarTotal_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CobrototalConEmplazamiento";
+            // 
+            // lbcobroconemplazamiento
+            // 
+            this.lbcobroconemplazamiento.AutoSize = true;
+            this.lbcobroconemplazamiento.Location = new System.Drawing.Point(205, 321);
+            this.lbcobroconemplazamiento.Name = "lbcobroconemplazamiento";
+            this.lbcobroconemplazamiento.Size = new System.Drawing.Size(27, 16);
+            this.lbcobroconemplazamiento.TabIndex = 4;
+            this.lbcobroconemplazamiento.Text = "NN";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(548, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "CobrototalsinEmplazamiento";
+            // 
+            // lbcobrosinemplazamiento
+            // 
+            this.lbcobrosinemplazamiento.AutoSize = true;
+            this.lbcobrosinemplazamiento.Location = new System.Drawing.Point(607, 321);
+            this.lbcobrosinemplazamiento.Name = "lbcobrosinemplazamiento";
+            this.lbcobrosinemplazamiento.Size = new System.Drawing.Size(27, 16);
+            this.lbcobrosinemplazamiento.TabIndex = 6;
+            this.lbcobrosinemplazamiento.Text = "NN";
+            // 
             // visualizartotaldeclarado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(951, 541);
+            this.Controls.Add(this.lbcobrosinemplazamiento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbcobroconemplazamiento);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnsalirIvisualizarTotal);
-            this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dtviewtodo);
             this.Name = "visualizartotaldeclarado";
             this.Text = "visualizartotaldeclarado";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.visualizartotaldeclarado_FormClosing);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.Load += new System.EventHandler(this.visualizartotaldeclarado_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtviewtodo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dtviewtodo;
         private System.Windows.Forms.Button btnsalirIvisualizarTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbcobroconemplazamiento;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbcobrosinemplazamiento;
     }
 }
