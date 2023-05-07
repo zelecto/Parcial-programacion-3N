67ec907648bbf866584139976a2483602af7fb55
zelecto <ca346364@gmail.com>
2023-05-06 19:42:11 -0500

    Entidades y datos

---
+++ Datos/ArchivosFormulario.cs
@@ -0,0 +1,80 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class ArchivosFormulario
+    {
+        string ruta = "Formulario.txt";
+
+        public bool Save(Formulario formulario)
+        {
+            var sw = new StreamWriter(ruta, true);
+            try
+            {
+                sw.WriteLine(formulario.ToString());
+                sw.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                sw.Close();
+                return false;
+            }
+
+        }
+
+
+        public List<Formulario> Consultar()
+        {
+            if (File.Exists(ruta))
+            {
+                List<Formulario> Formulario = new List<Formulario>();
+
+                try
+                {
+                    var sr = new StreamReader(ruta);
+                    while (!sr.EndOfStream)
+                    {
+                        Formulario.Add(Mapeador(sr.ReadLine()));
+                    }
+                    sr.Close();
+                    return Formulario;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+            return null;
+        }
+
+
+        public Formulario Mapeador(string linea)
+        {
+            Formulario formulario = new Formulario();
+            try
+            {
+                var auxi = linea.Split(';');
+
+                formulario.Id = int.Parse(auxi[0]);
+                formulario.Cedula = long.Parse(auxi[1]);
+                formulario.Nombre = auxi[2];
+                formulario.Fecha = DateTime.Parse(auxi[3]);
+                formulario.Sancion = bool.Parse(auxi[4]);
+                formulario.ValorDeclarado = double.Parse(auxi[5]);
+                formulario.Cobro = double.Parse(auxi[6]);
+                return formulario;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Entidades/Formulario.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Formulario
+    {
+        public Formulario()
+        {
+            Fecha = DateTime.Now;
+            Sancion = ValidarSancion();
+
+        }
+
+        public Formulario(int id, long cedula, string nombre, DateTime fecha, bool sancion, bool valorDeclarado, double cobro)
+        {
+            Id = id;
+            Cedula = cedula;
+            Nombre = nombre;
+            Fecha = fecha;
+            Sancion = sancion;
+            ValorDeclarado = valorDeclarado;
+            Cobro = cobro;
+        }
+
+        public int Id { get; set; }
+        public long Cedula { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+        public bool Sancion { get; set; }
+        public double ValorDeclarado { get; set; }
+        public double Cobro { get; set; }
+
+        public bool ValidarSancion()
+        {
+            if (Fecha.Day>10)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public double CalcularCobro()
+        {
+            if (Sancion)
+            {
+                return ValorDeclarado * Fecha.Day * 0.3;
+            }
+            return ValorDeclarado * Fecha.Day * 1160000;
+        }
+
+    }
+}
